04fb4b339d7089c52fbbc18af4acc62e6734d17f
slashhuang <627284482@qq.com>
2017-04-29 19:25:03 +0800

    update mongo partial

--- app/api/mongo/index.js
+++ app/api/mongo/index.js
@@ -3,24 +3,27 @@
  * @Author slashhuang
  * 17/4/26
  */
-
 const mongoose = require('mongoose');
-
 const { blogSchema,categorySchema } = require('./schema');
 //第一个参数是colleciton的名字
 const BlogModel = mongoose.model('Blog', blogSchema)
 const CategoryModel = mongoose.model('Category',categorySchema)
-const $_saveBlog = blog=>{
-	 return BlogModel.findOneAndUpdate({title:blog.title},blog,{
-        upsert:true
+const $_saveBlog = (blog)=>{
+    //去重 upsert
+    let condition = {title:blog.title}
+    let { id } = blog
+    if(id){
+        condition = { _id:transObjectId(id)}
+    }
+    blog.date  = new Date().toLocaleString()
+    return Blog.findOneAndUpdate(condition,blog,{
+        upsert:true,
+        new: true
      }).exec()
-       .then(_blog=>{
-		return {
-			status:1,
-			data:_blog
-		}
-	})
-}
+     .then(db_blog=>{
+         return { status:1,data:db_blog}
+     })
+ }
 const $_saveCategory = category=>{
 	return CategoryModel.findOneAndUpdate({name:category.name},category).then(_category=>{
 		return {
