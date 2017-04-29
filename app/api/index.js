fadd2615cda4c2d2c8a3eb695926c5c5b29f1aa4
slashhuang <627284482@qq.com>
2017-04-29 19:54:58 +0800

    update api server

--- app/api/index.js
+++ app/api/index.js
@@ -1,23 +1,27 @@
 /*
- * api server
+ * @Author slashhuang
+ * @Date 17/4/29
+ * api middleware
  */
-let Router = require('./ajax')
+
+const router = require('./ajax')
+
 module.exports=(ctx)=>{
- 	let { pathname,method } = ctx.reqCtx;
- 	let { resCtx,reqCtx } = ctx; 
- 	let { res } = ctx;
- 	return Promise.resolve({
- 		then:(resolve,reject)=>{
-		 	// 因为我们对method没有过滤
-		 	if(pathname.match('action')){
-		 		return Router.routes(ctx).then(val=>{
-	 				resCtx.body = JSON.stringify(val)
-	 				resCtx.headers = Object.assign(resCtx.headers,{
-					 	"Content-Type":"application/json"
-					})
-					resolve()
-	 			})
-		 	}
-		 	resolve()
- 	}})
+ 	let { resCtx,reqCtx } = ctx;
+ 	let { pathname } = reqCtx;
+ 	if(!pathname.match(/\.action/)){
+ 		//let it pass
+ 		return Promise.resolve()
+ 	}
+	return router.routes(ctx).then(val=>{
+		if(val){
+			resCtx.statusCode=200
+			resCtx.headers = Object.assign(resCtx.headers,{
+				 "Content-Type":"application/json"})
+			resCtx.body = JSON.stringify(val);
+		}
+	}).catch(err=>{
+		resCtx.statusCode=400
+		resCtx.body =`${err.name} + ${err.stack}`;
+	})
  }
